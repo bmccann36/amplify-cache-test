a5618c058bd77539eeed792b9079568162db8da5
brianmccann <bmccann36@gmail.com>
2022-11-22 07:40:45 -0500

    test cache

--- compute-hashes.sh
+++ compute-hashes.sh
@@ -1,5 +1,5 @@
 
-
+echo 'contents of node_modules: '
 ls node_modules
 
 PREV_HASH=$(envCache --get packageLockSha)
@@ -7,8 +7,11 @@ CURR_HASH="$(sha1sum package-lock.json | cut -d " " -f 1 )"
 
 
 if [ "$PREV_HASH" = "$CURR_HASH" ]; then
-  echo "package-lock has not changed since last build"
+  printf "\n ** package-lock has not changed since last build ** \n"
+  export SHOULD_NPM_INSTALL=true
+else
+  export SHOULD_NPM_INSTALL=false
 fi
 
 
-envCache --set packageLockSha $CURR_HASH
+envCache --set packageLockSha "$CURR_HASH"
