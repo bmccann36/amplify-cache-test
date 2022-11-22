932d3c736d5b4415dcd06aac48a60cfc1827d82f
brianmccann <bmccann36@gmail.com>
2022-11-22 08:27:46 -0500

    testing cache

---
+++ check-hashes-for-npm.sh
@@ -0,0 +1,19 @@
+
+echo 'contents of node_modules: '
+ls node_modules
+
+PREV_HASH=$(envCache --get packageLockSha)
+CURR_HASH="$(sha1sum package-lock.json | cut -d " " -f 1 )"
+
+
+if [ "$PREV_HASH" = "$CURR_HASH" ]; then
+  echo "---------------------------------------------------------"
+  echo "** PACKAGE-LOCK.JSON FILE NOT CHANGED SINCE LAST BUILD **"
+  echo "---------------------------------------------------------"
+  export SHOULD_NPM_INSTALL=1
+else
+  export SHOULD_NPM_INSTALL=0
+fi
+
+
+envCache --set packageLockSha "$CURR_HASH"
---
+++ scratch.sh
@@ -0,0 +1,4 @@
+
+
+
+if [ "$SHOULD_NPM_INSTALL" -eq 1 ]; then echo "should install"; fi
