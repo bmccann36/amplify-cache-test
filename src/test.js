b048a65215a05f0f95eabe87a3bd434996835c1a
brianmccann <bmccann36@gmail.com>
2022-11-22 08:45:30 -0500

    test cache

--- src/test.js
+++ src/test.js
@@ -1,13 +1,19 @@
 import chalk from 'chalk'
-import winston from "winston";
+import winston, {transports} from "winston";
 
 const logger = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
   defaultMeta: {service: 'user-service'},
   transports: [
-
-  ],
+    new transports.Console({
+      level: 'info',
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      )
+    })
+  ]
 });
 
 logger.info('hi from winston')
