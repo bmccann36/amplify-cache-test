c031fde14bf76e1a2644296b9e84cdc4f0d9ae77
brianmccann <bmccann36@gmail.com>
2022-11-22 07:07:58 -0500

    try setting cache

--- compute-hashes.sh
+++ compute-hashes.sh
@@ -1,3 +1,6 @@
-PACKAGE_LOCK_SHA1="$(sha1sum package-lock.json | cut -d " " -f 1 )"
 
-echo $PACKAGE_LOCK_SHA1
+
+ls node_modules
+echo $(envCache --get packageLockSha)
+export PACKAGE_LOCK_SHA1="$(sha1sum package-lock.json | cut -d " " -f 1 )"
+envCache --set packageLockSha $PACKAGE_LOCK_SHA1
