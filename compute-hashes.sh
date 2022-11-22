50049755ba94bf1da0a3bdb25b658857f3c7636b
brianmccann <bmccann36@gmail.com>
2022-11-22 07:15:02 -0500

    try setting cache

--- compute-hashes.sh
+++ compute-hashes.sh
@@ -1,6 +1,14 @@
 
 
 ls node_modules
-echo $(envCache --get packageLockSha)
-export PACKAGE_LOCK_SHA1="$(sha1sum package-lock.json | cut -d " " -f 1 )"
-envCache --set packageLockSha $PACKAGE_LOCK_SHA1
+
+PREV_HASH=$(envCache --get packageLockSha)
+CURR_HASH="$(sha1sum package-lock.json | cut -d " " -f 1 )"
+
+
+if [ "$PREV_HASH" = "$CURR_HASH" ]; then
+  echo "package-lock has not changed since last build"
+fi
+
+
+envCache --set packageLockSha $CURR_HASH
